8de399283cec966e7d2bf3074260f61e07fb9ad5
Stephen FitzSimon <stephen.fitzsimon@gmail.com>
2022-12-13 18:55:59 -0600

    html template rendering

--- tutorial1.py
+++ tutorial1.py
@@ -1,23 +1,30 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+#can pass a parameter to render in the template
 @app.route("/") #use a decorator to note root folder for the project
 def home():
     # make a home page
-    return "<h1>Hello world</h1> <p>And here is some paragraph tagged text</p>"
+    # render_template looks in the template folder to produce the html
+    # note that the folder must be called template!
+    #recall that {{}} will be replaced with the parameter
+    return render_template("index.html", content = ["tim", "joe", "bill"])
 
-# tag the parameter for the function in the decorator
-# so that the parameter can be passed by the URL
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}"
 
-# make an admin page and redirect users who do not have
-# credentials
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+
+# # tag the parameter for the function in the decorator
+# # so that the parameter can be passed by the URL
+# @app.route("/<name>")
+# def user(name):
+#     return f"Hello {name}"
+
+# # make an admin page and redirect users who do not have
+# # credentials
+# @app.route("/admin")
+# def admin():
+#     # send them to the hello user page, but pass admin as the
+#     return redirect(url_for("user", name="Admin"))
 
 if __name__ == "__main__":
     app.run()
