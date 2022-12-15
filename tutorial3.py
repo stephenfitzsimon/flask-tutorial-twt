cf7ed3fc5d3a089f5d1787950f26856d14b3c4a1
Stephen FitzSimon <stephen.fitzsimon@gmail.com>
2022-12-15 15:14:02 -0600

    post and get processing

--- tutorial3.py
+++ tutorial3.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -7,9 +7,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", content = "Testing")
 
-@app.route("/test") 
-def test():
-    return render_template("new.html")
+@app.route("/login", methods = ["POST", "GET"]) 
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr = user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run(debug = True)
